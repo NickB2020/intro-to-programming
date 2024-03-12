8cec5f9b2a41416519955532a6ac058343b0b605
NickB2020 <klaus@neo.rr.com>
2024-03-12 16:53:05 -0400

    End of Tuesday

--- src/week2/app/frontend/src/app/pages/counter/state/actions.ts
+++ src/week2/app/frontend/src/app/pages/counter/state/actions.ts
@@ -1,10 +1,15 @@
-import { createActionGroup, emptyProps } from '@ngrx/store';
+import { createActionGroup, emptyProps, props } from '@ngrx/store';
+import { CounterState } from '.';
 
-export const CounterAction = createActionGroup({
+export type CountByValues = 1 | 3 | 5;
+export const CounterActions = createActionGroup({
   source: 'Counter Component Events',
   events: {
     'Incremented The Count': emptyProps(),
     'Decremented The Count': emptyProps(),
     'Count was Reset': emptyProps(),
+    'Count By Changed': props<{ payload: CountByValues }>(),
+    'Counter Entered': emptyProps(),
+    State: props<{ payload: CounterState }>(),
   },
 });
--- src/week2/app/frontend/src/app/pages/counter/state/index.ts
+++ src/week2/app/frontend/src/app/pages/counter/state/index.ts
@@ -1,24 +1,36 @@
 import { createFeature, createReducer, on } from '@ngrx/store';
-import { CounterAction } from './actions';
+import { CountByValues, CounterActions } from './actions';
 
 export interface CounterState {
   current: number;
+  by: CountByValues;
 }
 
-const initialState: CounterState = {
+export const initialState: CounterState = {
   current: 0,
+  by: 1,
 };
 
 export const counterFeature = createFeature({
   name: 'counter',
   reducer: createReducer(
     initialState,
-    on(CounterAction.incrementedTheCount, (state) => ({
-      current: state.current + 1,
+    on(CounterActions.incrementedTheCount, (state) => ({
+      ...state,
+      current: state.current + state.by,
     })),
-    on(CounterAction.decrementedTheCount, (state) => ({
-      current: state.current - 1,
+    on(CounterActions.decrementedTheCount, (state) => ({
+      ...state,
+      current: state.current - state.by,
     })),
-    on(CounterAction.countWasReset, () => initialState)
+    on(CounterActions.countWasReset, (state) => ({
+      ...state,
+      current: initialState.current,
+    })),
+    on(CounterActions.countByChanged, (state, action) => ({
+      ...state,
+      by: action.payload,
+    })),
+    on(CounterActions.state, (_, a) => a.payload)
   ),
 });
